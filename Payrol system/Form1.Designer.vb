da59e7587a6f0b60b285895c08f9a30fe37bab5c
wambui <wambuistephen@zetech.ac.ke>
2024-10-20 17:59:50 +0300

    payroll

---
+++ Payrol system/Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
+        TextBox12 = New TextBox()
+        TextBox11 = New TextBox()
+        TextBox10 = New TextBox()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        GroupBox3 = New GroupBox()
+        TextBox7 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        TextBox4 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.Orange
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(23, 11)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(354, 183)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Payments Rate"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(173, 146)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(175, 33)
+        TextBox3.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(173, 94)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(175, 33)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(173, 39)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(175, 33)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(22, 138)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(135, 30)
+        Label3.TabIndex = 2
+        Label3.Text = "Worked Days"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(16, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(151, 30)
+        Label2.TabIndex = 1
+        Label2.Text = "Rates Per Hour"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(22, 33)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(145, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Worked Hours"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = Color.Orange
+        GroupBox2.Controls.Add(TextBox12)
+        GroupBox2.Controls.Add(TextBox11)
+        GroupBox2.Controls.Add(TextBox10)
+        GroupBox2.Controls.Add(Label9)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Location = New Point(464, 19)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(275, 188)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.Location = New Point(43, 150)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(194, 22)
+        TextBox12.TabIndex = 6
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(43, 95)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(194, 22)
+        TextBox11.TabIndex = 5
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(43, 40)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(194, 22)
+        TextBox10.TabIndex = 4
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(67, 120)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(132, 30)
+        Label9.TabIndex = 3
+        Label9.Text = "NET SALARY"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(67, 65)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(141, 30)
+        Label8.TabIndex = 2
+        Label8.Text = "DEDUCTIONS"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(67, 10)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(159, 30)
+        Label7.TabIndex = 1
+        Label7.Text = "GROSS SALARY"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.BackColor = Color.Orange
+        GroupBox3.Controls.Add(TextBox7)
+        GroupBox3.Controls.Add(TextBox6)
+        GroupBox3.Controls.Add(TextBox5)
+        GroupBox3.Controls.Add(Label6)
+        GroupBox3.Controls.Add(Label5)
+        GroupBox3.Controls.Add(Label4)
+        GroupBox3.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox3.Location = New Point(23, 226)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(354, 149)
+        GroupBox3.TabIndex = 1
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Diductions"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(173, 111)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(175, 33)
+        TextBox7.TabIndex = 6
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(173, 71)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(175, 33)
+        TextBox6.TabIndex = 5
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(173, 28)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(175, 33)
+        TextBox5.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(88, 102)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(60, 30)
+        Label6.TabIndex = 3
+        Label6.Text = "NHIF"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(87, 71)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(61, 30)
+        Label5.TabIndex = 2
+        Label5.Text = "NSSF"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(71, 28)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(77, 30)
+        Label4.TabIndex = 1
+        Label4.Text = "P.A.Y.E"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(626, 451)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 28)
+        Button1.TabIndex = 2
+        Button1.Text = "EXIT"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(427, 450)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 28)
+        Button2.TabIndex = 3
+        Button2.Text = "RESET"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(217, 451)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(105, 28)
+        Button3.TabIndex = 4
+        Button3.Text = "COMPUTE"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(51, 192)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(120, 30)
+        Label10.TabIndex = 5
+        Label10.Text = "GROSS PAY"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label11.Location = New Point(7, 380)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(173, 25)
+        Label11.TabIndex = 6
+        Label11.Text = "TOTAL DEDUCTION"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label12.Location = New Point(46, 408)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(134, 30)
+        Label12.TabIndex = 7
+        Label12.Text = "ALLOWANCE"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(196, 200)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(175, 22)
+        TextBox4.TabIndex = 6
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(196, 380)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(175, 22)
+        TextBox8.TabIndex = 8
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(196, 416)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(175, 22)
+        TextBox9.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 14F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(746, 485)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox4)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Font = New Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ForeColor = SystemColors.MenuHighlight
+        Name = "Form1"
+        Text = "PAYROLL SYSTEM"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Public WithEvents TextBox3 As TextBox
+    Public WithEvents TextBox2 As TextBox
+    Public WithEvents TextBox1 As TextBox
+    Public WithEvents TextBox12 As TextBox
+    Public WithEvents TextBox11 As TextBox
+    Public WithEvents TextBox10 As TextBox
+    Public WithEvents TextBox7 As TextBox
+    Public WithEvents TextBox6 As TextBox
+    Public WithEvents TextBox5 As TextBox
+    Public WithEvents TextBox4 As TextBox
+    Public WithEvents TextBox8 As TextBox
+    Public WithEvents TextBox9 As TextBox
+
+End Class
---
+++ Payrol system/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private RatePerHour As Object
+
+    ' Define rates as constants
+    Private Const PAYE_RATE As Double = 0.16
+    Private Const NSSF_RATE As Double = 0.03
+    Private Const NHIF_RATE As Double = 0.07
+    Private Const ALLOWANCE_RATE As Double = 0.25
+
+
+    Public Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Declare variables
+        Dim workedHours As Double
+        Dim ratePerHour As Double
+        Dim workedDays As Integer
+        Dim grossPay As Double
+        Dim paye As Double
+        Dim nssf As Double
+        Dim nhif As Double
+        Dim totalDeductions As Double
+        Dim allowance As Double
+        Dim grossSalary As Double
+        Dim Deduction As Double
+        Dim netSalary As Double
+
+        ' Get input values from the user
+
+        If Not Double.TryParse(TextBox1.Text, workedHours) Then
+            MessageBox.Show("invalid Enter valid hours worked")
+            Exit Sub
+        End If
+
+        If Not Double.TryParse(TextBox2.Text, ratePerHour) Then
+            MessageBox.Show("incorrect hours input new hour rate")
+            Exit Sub
+        End If
+
+        If Not Integer.TryParse(TextBox3.Text, workedDays) Then
+            MessageBox.Show("incorrect days")
+            Exit Sub
+        End If
+
+
+
+        ' Calculate Gross Pay
+        grossPay = workedHours * ratePerHour * workedDays
+        TextBox4.Text = grossPay.ToString("C")
+
+        'calculate diduction based on the rate
+        paye = grossPay * PAYE_RATE
+        nssf = grossPay * NSSF_RATE
+        nhif = grossPay * NHIF_RATE
+
+        'display deduction to their respective boxes
+        TextBox5.Text = paye.ToString("C")
+        TextBox6.Text = nssf.ToString("C")
+        TextBox7.Text = nhif.ToString("C")
+
+
+        ' Calculate Total Deductions
+        totalDeductions = paye + nssf + nhif
+        TextBox8.Text = totalDeductions.ToString("C")
+
+        'to calculate allowance4
+        allowance = grossPay * ALLOWANCE_RATE
+        TextBox9.Text = allowance.ToString("C")
+
+        'Calculate Gross Salary
+        grossSalary = grossPay + allowance
+        TextBox10.Text = grossSalary.ToString("C")
+
+        'Decuctions
+        Deduction = totalDeductions
+        TextBox11.Text = Deduction.ToString("C")
+
+        ' Calculate Net Salary
+        netSalary = grossPay - totalDeductions + allowance
+        TextBox12.Text = netSalary.ToString("C")
+    End Sub
+
+    Public Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        TextBox10.Text = ""
+        TextBox11.Text = ""
+        TextBox12.Text = ""
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
+
+
+End Class
